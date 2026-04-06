d6d7448418106dbfda0ac9b950efd04223e8ff46
西郷隆盛 <teon22dean@gmail.com>
2026-04-06 21:54:50 +0900

    日本語組版

--- ato/src/main.tsx
+++ ato/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router";
 import App from "./app/App.tsx";
+import TypesetDemo from "./app/TypesetDemo.tsx";
 import { I18nProvider, isLocale, type Locale } from "./i18n";
 import "./styles/index.css";
 
@@ -25,6 +26,7 @@ createRoot(document.getElementById("root")!).render(
     <Routes>
       <Route path="/" element={<LocaleRoute locale="ja" />} />
       <Route path="/:locale" element={<LocaleRouteFromParam />} />
+      <Route path="/typeset" element={<TypesetDemo />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
